eeff951c596b55c66ac6ce7cbf172234d548590d
guihenrry <guilhermehenry60@gmail.com>
2019-09-22 02:02:38 -0300

    removido button teste

--- js/script.js
+++ js/script.js
@@ -2,11 +2,3 @@ import Pomodoro from './module/Pomodoro.js';
 
 const pomodoro = new Pomodoro(1);
 pomodoro.init()
-
-const buttonTeste = document.querySelector('[data-pomodoro="teste"]');
-buttonTeste.addEventListener('click', () => {
-  pomodoro.currentMinute = 0;
-  pomodoro.currentSecond = 3;
-
-  pomodoro.start();
-});
